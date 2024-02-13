d78fe2acccea196a79ef0535c0caa253d6d298da
hillary-254 <123866475+hillary-254@users.noreply.github.com>
2024-02-13 19:38:09 +0300

    installed the bcrypt encryption package, created a password comparison method that compares hashed password and a pre-save hooks that checks if there any modification or new data which ten hashes the password then proceeds to save

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 //define the schema
 const userSchema = mongoose.Schema({
@@ -20,6 +21,21 @@ const userSchema = mongoose.Schema({
         default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
     }
 }, {timestamps: true})
+
+//User method that does password comparison for verification
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+//pre-save hook that runs the function before the function is finally saved
+//if no modifications has been done, it proceeds to save using the next()
+userSchema.pre('save', async function(next) {
+    if(this.isModified){
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    }
+    next()
+})
 
 //create a model
 const User = mongoose.model('User', userSchema)
